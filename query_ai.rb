49fe918836d2b3fb6e1b61e736144f04466ad638
Tim Hosey <insertcointheater@gmail.com>
2025-05-09 13:35:26 -0700

    added first options and externalized config

--- query_ai.rb
+++ query_ai.rb
@@ -1,11 +1,15 @@
 require 'ollama-ai'
 require 'json'
 require 'benchmark'
+require 'yaml'
+require 'optparse'
 
 require_relative 'conversion_functions'
 
-OLLAMA_SERVER = 'http://192.168.0.60:11434'
-OLLAMA_MODEL = 'granite3.3:8b'
+config = YAML.load_file('config.yml')
+
+OLLAMA_SERVER = config['server']
+OLLAMA_MODEL = config['model']
 
 # Types: journalctl, file
 LOG_TYPE = 'file'
@@ -15,13 +19,27 @@ LOG_FILE = '/Users/timhosey/Downloads/journalctl.txt'
 
 # Only if LOG_TYPE = 'journalctl'
 # Number of hours back to pull logs
+# TODO: Needs to be externalized somehow
 LOG_JOURNAL_AGE_HRS = 24
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: query_ai.rb --type [journalctl,file] --filepath PATH_IF_FILE_TYPE"
+  opts.on("--type TYPE", "Specify a type. Supported: [journalctl, file]") do |t|
+    options[:type] = t
+  end
+
+  opts.on("-h", "--help", "Prints this dialog.") do
+    puts opts
+    exit
+  end
+end.parse!
 
 # Get timestamp for output naming
 current_timestamp = Time.now.strftime("%Y%m%d-%H%M%S")
 
 # Output settings
-OUTPUT_PATH = './output'
+OUTPUT_PATH = config['output']['path']
 OUTPUT_FILE = "log_#{LOG_TYPE}_#{current_timestamp}"
 
 case(LOG_TYPE)
@@ -39,7 +57,7 @@ end
 ANALYSIS_PROMPT = <<~PROMPT
   You are a helpful IT assistant. Based on the below log, can you call out any WARNING, ERROR, CRITICAL, or FATAL log entries with their timestamp in PST, and give a brief synopsis of what that log calls out. If there are none that match this, let the user know and don't worry about the synopsis.
   
-  Then, can you summarize the most concerning log entries, if any, and provide specifically the entries and their line numbers? Call out specifically any WARNING, ERROR, CRITICAL, or FATAL errors immediately, with appropriate emphasis given to the severity of these entries. Give specific recommendations on remediations where possible.
+  Then, can you summarize the most concerning log entries, if any, and provide specifically the entries and their line numbers? Call out specifically any WARNING, ERROR, CRITICAL, or FATAL errors immediately, with appropriate emphasis given to the severity of these entries. Give specific recommendations on remediations where possible. Make a brief synopsis of the overall status of the logs, especially if there's nothing to be concerned about.
   
   Use only plaintext for formatting. Remember to treat the user as a professional with a higher than average understanding of the subject matter.
   
