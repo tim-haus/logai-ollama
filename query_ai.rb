09fe165a775ae2cb7463d2d7b67c2e69c08f9c87
Tim Hosey <insertcointheater@gmail.com>
2025-05-09 14:01:00 -0700

    Added variables and externalization

--- query_ai.rb
+++ query_ai.rb
@@ -11,18 +11,10 @@ config = YAML.load_file('config.yml')
 OLLAMA_SERVER = config['server']
 OLLAMA_MODEL = config['model']
 
-# Types: journalctl, file
-LOG_TYPE = 'file'
-
-# Only if LOG_TYPE = 'file'
-LOG_FILE = '/Users/timhosey/Downloads/journalctl.txt'
-
 # Only if LOG_TYPE = 'journalctl'
 # Number of hours back to pull logs
 # TODO: Needs to be externalized somehow
 LOG_JOURNAL_AGE_HRS = 24
-
-puts "CONFIG: #{config}"
 
 options = {}
 parser = OptionParser.new do |opts|
@@ -49,68 +41,78 @@ rescue OptionParser::InvalidOption => e
   exit 1
 end
 
+# Parameter validation
+if options[:log_type].nil?
+  puts "Error: --type is required."
+  puts parser
+  exit 1
+elsif (options[:log_file].nil? && options[:log_type] == 'file')
+  puts "Error: --file is required if --type is set to file."
+  puts parser
+  exit 1
+end
+
 # Example use of parsed options
 puts "Options: #{options.inspect}"
 
-# # Get timestamp for output naming
-# current_timestamp = Time.now.strftime("%Y%m%d-%H%M%S")
+log_content = ''
 
-# # Output settings
-# OUTPUT_PATH = config['output']['path']
-# OUTPUT_FILE = "log_#{LOG_TYPE}_#{current_timestamp}"
+case(options[:log_type])
+when 'journalctl'
+  # Pull journalctl
+  log_content = `journalctl --since "#{LOG_JOURNAL_AGE_HRS} hours ago" --no-pager`
+when 'file'
+  # Read file for submission.
+  log_content = File.read(options[:log_file])
+  puts 'Log file loaded.'
+else
+  exit 'something went wrong'
+end
 
-# case(LOG_TYPE)
-# when 'journalctl'
-#   # Pull journalctl
-#   log_content = `journalctl --since "#{LOG_JOURNAL_AGE_HRS} hours ago" --no-pager`
-# when 'file'
-#   # Read file for submission.
-#   log_content = File.read(LOG_FILE)
-#   puts 'Log file loaded.'
-# else
-#   exit 'something went wrong'
-# end
+output_variables = {
+  TIMESTAMP: Time.now.strftime("%Y%m%d-%H%M%S"),
+  LOG_TYPE: options[:log_type]
+}
 
-# ANALYSIS_PROMPT = <<~PROMPT
-#   You are a helpful IT assistant. Based on the below log, can you call out any WARNING, ERROR, CRITICAL, or FATAL log entries with their timestamp in PST, and give a brief synopsis of what that log calls out. If there are none that match this, let the user know and don't worry about the synopsis.
-  
-#   Then, can you summarize the most concerning log entries, if any, and provide specifically the entries and their line numbers? Call out specifically any WARNING, ERROR, CRITICAL, or FATAL errors immediately, with appropriate emphasis given to the severity of these entries. Give specific recommendations on remediations where possible. Make a brief synopsis of the overall status of the logs, especially if there's nothing to be concerned about.
-  
-#   Use only plaintext for formatting. Remember to treat the user as a professional with a higher than average understanding of the subject matter.
-  
-#   Here's the log contents:
-#   #{log_content}
-# PROMPT
+# Output settings
+OUTPUT_PATH = config['output']['path']
+OUTPUT_FILE = config['output']['file_template'] % output_variables
 
-# puts "Prompt prepared. Connecting to LLM using model #{OLLAMA_MODEL}..."
+prompt_variables = {
+  TIMESTAMP: Time.now.strftime("%Y%m%d-%H%M%S"),
+  LOG_CONTENT: log_content
+}
 
+ANALYSIS_PROMPT = config['prompt'] % config_variables
 
-# client = Ollama.new(
-#   credentials: { address: OLLAMA_SERVER },
-#   options: { server_sent_events: true }
-# )
+puts "Prompt prepared. Connecting to LLM using model #{OLLAMA_MODEL}..."
 
+client = Ollama.new(
+  credentials: { address: OLLAMA_SERVER },
+  options: { server_sent_events: true }
+)
 
-# puts 'Connected. Submitting log to LLM for analysis. Waiting while LLM is processing...'
+puts 'Connected. Submitting log to LLM for analysis. Waiting while LLM is processing...'
 
-# result = ''
-# process_time = Benchmark.realtime do
-#   result = client.generate(
-#     { model: OLLAMA_MODEL,
-#       prompt: ANALYSIS_PROMPT,
-#       stream: false }
-#   )
-# end
+result = ''
+process_time = Benchmark.realtime do
+  result = client.generate(
+    { model: OLLAMA_MODEL,
+      prompt: ANALYSIS_PROMPT,
+      stream: false }
+  )
+end
 
-# puts "Completed in #{process_time.round(3)} seconds."
+puts "Completed in #{process_time.round(3)} seconds."
 
-# output_content = remove_tag_and_contents(result[0]['response'], 'think')
+output_content = remove_tag_and_contents(result[0]['response'], 'think')
 
-# # We strip out the think tag since it's worthless to what we want sent back to us.
-# puts "\nRESPONSE:\n\n"
-# puts output_content
-# puts "\n"
+# We strip out the think tag since it's worthless to what we want sent back to us.
+puts "\nRESPONSE:\n\n"
+puts output_content
+puts "\n"
 
-# Dir.mkdir(OUTPUT_PATH) unless Dir.exist?(OUTPUT_PATH)
-# File.write("#{OUTPUT_PATH}/#{OUTPUT_FILE}", output_content)
-# puts "Complete. Log saved to #{OUTPUT_PATH}/#{OUTPUT_FILE}."
+Dir.mkdir(OUTPUT_PATH) unless Dir.exist?(OUTPUT_PATH)
+File.write("#{OUTPUT_PATH}/#{OUTPUT_FILE}", output_content)
+
+puts "Complete. Log saved to #{OUTPUT_PATH}/#{OUTPUT_FILE}."
